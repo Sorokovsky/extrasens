dfe96e5d884433281aa5757ef35a47c13e9894ab
Sorokovsky <Sorokovskys@ukr.net>
2022-06-13 17:33:42 +0300

    update

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ if(!$_SESSION['answers']){
         <select name="di" id="">
             <?php for($i = 1; $i < 100; $i += 10){ 
             ?>
-            <option <?php?> value="<?= $i ?>"><?= $i ?>-<?= $i + 9 ?></option>;
+            <option <?php if($_SESSION['try'] >= 0 AND $_SESSION['rand'] >= $i AND $_SESSION['rand'] <= $i + 9){echo "selected";}?> value="<?= $i ?>"><?= $i ?>-<?= $i + 9 ?></option>;
             <?php } ?>
         </select>
         <button type="submit">Обрати діапазон</button>
@@ -35,10 +35,11 @@ if(!$_SESSION['answers']){
         $di = $_GET['di'];
         if($di <= $_SESSION['rand'] AND ($di + 9) >= $_SESSION['rand']){
             echo 'Ви вірно вибрали діапазон';
+            echo '<a href="./number.php">Choose number</a>';
         }elseif ($_SESSION['try'] >= 3) {
             echo "Ви програли";
             echo '<pre>';
-            echo ``;
+            echo '<a href="./again.php">Play again</a>';
         }else{
             $_SESSION['try']++;
             echo 'Ви не вірно вибрали діапазон';
