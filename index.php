f2773d561460ef4208b5dc916c993b9f50cda60b
Sorokovsky <Sorokovskys@ukr.net>
2022-06-13 19:43:22 +0300

    update

--- index.php
+++ index.php
@@ -4,13 +4,10 @@ if(!$_SESSION['rand']){
     $_SESSION['rand'] = rand(1, 100);
 }
 if(!$_SESSION['try']){
-    $_SESSION['try'] = 0;
+    $_SESSION['try'] = 1;
 }
 if(!$_SESSION['answers']){
-    $_SESSION['answers'] = [];
-}
-if(!$_SESSION['di']){
-    $_SESSION['di'] = 1;
+    $_SESSION['answers'] = [91];
 }
 ?>
 <!DOCTYPE html>
@@ -22,13 +19,12 @@ if(!$_SESSION['di']){
     <title>Екстрасенс розширена версія</title>
 </head>
 <body>
-    <p>Оберіть діапазон вибору</p>
-    <?php echo $_SESSION['rand']; ?>
+    <p>Оберіть діапазон вибору</p> 
     <form action="index.php">
         <select name="di" id="">
             <?php for($i = 1; $i < 100; $i += 10){ 
             ?>
-            <option <?php if($_SESSION['try'] >= 0 AND $_SESSION['rand'] >= $i AND $_SESSION['rand'] <= $i + 9){echo "selected";}?> value="<?= $i ?>"><?= $i ?>-<?= $i + 9 ?></option>;
+            <option <?php if($_SESSION['try'] > 1 AND $_SESSION['rand'] >= $i AND $_SESSION['rand'] <= $i + 9){echo "selected";}?> value="<?= $i ?>"><?= $i ?>-<?= $i + 9 ?></option>;
             <?php } ?>
         </select>
         <button type="submit">Обрати діапазон</button>
@@ -39,8 +35,9 @@ if(!$_SESSION['di']){
         if($di <= $_SESSION['rand'] AND ($di + 9) >= $_SESSION['rand']){
             echo 'Ви вірно вибрали діапазон';
             $_SESSION['di'] = $di;
-            echo '<a href="./number.php">Choose number</a>';
-        }elseif ($_SESSION['try'] >= 3) {
+            echo '<pre>';
+            echo '<a href="./number.php?di='.$di.'">Choose number</a>';
+        }elseif ($_SESSION['try'] > 3) {
             echo "Ви програли";
             echo '<pre>';
             echo '<a href="./again.php">Play again</a>';
--- number.php
+++ number.php
@@ -1,5 +1,11 @@
 <?php 
 session_start();
+if(!$_SESSION['di'] AND $_GET){
+    $_SESSION['di'] = $_GET['di'];
+}
+if($_GET['numbers']){
+    $_SESSION['answers'][count($_SESSION['answers'])] = $_GET['numbers'];
+}
 ?>
 <!DOCTYPE html>
 <html lang="uk">
@@ -15,12 +21,44 @@ session_start();
     <form action="./number.php" method="get">
         <p>Choose number</p>
         <select name="numbers">
-            <?php for ($i = $_SESSION['di']; $i <= ($_SESSION['di'] + 9); $i++) { 
-                        for($g = $_SESSION['answers']; $g < count($_SESSION['ansvers']); $g++){?>
-                <option <?php if($i == $_SESSION['answers']){echo 'disabled';} ?> value="<?= $i ?>"><?= $i ?></option>
-            <?php } }?>
+            <?php for ($i = $_SESSION['di']; $i <= ($_SESSION['di'] + 9); $i++) {
+                        $dis = false; 
+                        for($g = 0; $g < count($_SESSION['answers']); $g++){
+                            if($_SESSION['answers'][$g] == $i){
+                                $dis = true;
+                                break;
+                            }
+                        } ?>
+                        <?php if($dis === true){echo "disabled";}?>
+                    <option <?php if($dis === true){echo "disabled";}?> value="<?= $i ?>"><?= $i ?></option>
+            <?php }?>
         </select>
         <button type="submit">Choose</button>
     </form>
+    <?php 
+    if($_GET['numbers']){
+        if($_SESSION['try'] > 3){
+            echo "Ви програли";
+            echo '<pre>';
+            echo '<a href="./again.php">Play again</a>';
+        }else if($_SESSION['rand'] == $_GET['numbers']){
+            echo $_SESSION['rand'];
+            if($_SESSION['try'] == 0){
+                echo 'Ви справжній екстрасенс';
+            }else{
+                echo 'Ви вгадаои число '.$_SESSION['try'].' з разу';
+            }
+            echo '<pre>';
+            echo '<a href="./again.php">Play again</a>';
+        }else{
+            $_SESSION['try'] = $_SESSION['try'] + 1;
+            if($_SESSION['rand'] > $_SESSION['numbers']){
+                echo 'загадане число менше '.$_SESSION['numbers'].'.';
+            }else{
+                echo 'загадане число більше '.$_SESSION['numbers'].'.';
+            }
+        }
+    }
+    ?>
 </body>
 </html>
