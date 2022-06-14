d8e2fdb4e993a008a31b5a486f27a321ef988a7c
Sorokovsky <Sorokovskys@ukr.net>
2022-06-14 20:02:15 +0300

    update

--- index.php
+++ index.php
@@ -1,56 +1,52 @@
-<?php
+<?php 
 session_start();
+if(!$_SESSION['try']){
+    $_SESSION['try'] = 0;
+}
+if(!$_SESSION['answers']){
+    $_SESSION['answers'] = [];
+}
 if(!$_SESSION['rand']){
     $_SESSION['rand'] = rand(1, 100);
 }
-if(!$_SESSION['try']){
-    $_SESSION['try'] = 1;
-}
-if(!$_SESSION['answers']){
-    $_SESSION['answers'] = [91];
-}
-if($_GET){
-    if($di <= $_SESSION['rand'] AND ($di + 9) >= $_SESSION['rand']){
-    
-    
-    }else{
-        $_SESSION['try'] = $_SESSION['try'] + 1;
-    }
-}
-?>
+ ?>
 <!DOCTYPE html>
-<html lani="en">
+<html lang="uk">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edie">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Екстрасенс розширена версія</title>
+    <title>Extrasense extra</title>
 </head>
 <body>
-    <p>Оберіть діапазон вибору</p> 
-    <form action="index.php">
-        <select name="di" id="">
-            <?php for($i = 1; $i < 100; $i += 10){ 
-            ?>
-            <option <?php if($_SESSION['try'] > 1 AND $_SESSION['rand'] >= $i AND $_SESSION['rand'] <= $i + 9){echo "selected";}?> value="<?= $i ?>"><?= $i ?>-<?= $i + 9 ?></option>;
+    <form action="./index.php">
+        <p>Виберіть правильний діапазон</p>
+        <?php if($_SESSION['try'] < 3){?>
+            <select name="di" id="">
+            <?php
+                for($i = 1; $i <= 100; $i = $i + 10){?>
+                    <option <?php if($_GET AND $_SESSION['try'] >= 0 AND $_SESSION['rand'] >= $i AND $_SESSION['rand'] <= $i + 9){
+                        echo  'selected';
+                    } ?> value="<?= $i ?>"><?= $i?>-<?= $i + 9 ?></option>
             <?php } ?>
         </select>
-        <button type="submit">Обрати діапазон</button>
+        <button type="submit">Обрати</button>
+        <?php } ?>
     </form>
     <?php
     if($_GET){
-        $di = $_GET['di'];
-        if($di <= $_SESSION['rand'] AND ($di + 9) >= $_SESSION['rand']){
-            echo 'Ви вірно вибрали діапазон';
-            $_SESSION['di'] = $di;
+        if($_SESSION['try'] >= 3){
+            echo 'Ви програли';
             echo '<pre>';
-            echo '<a href="./number.php?di='.$di.'">Choose number</a>';
-        }elseif ($_SESSION['try'] > 3) {
-            echo "Ви програли";
+            echo '<a href="./again.php">Грати знову</a>';
+        }else if($_SESSION['rand'] >= $_GET['di'] AND $_SESSION['rand'] <= $_GET['di'] + 9){
+            echo 'Ви вірно обрали діапазон';
+            $_SESSION['di'] = $_GET['di'];
             echo '<pre>';
-            echo '<a href="./again.php">Play again</a>';
+            echo '<a href="./number.php">Оберіть число</a>';
         }else{
-            echo 'Ви не вірно вибрали діапазон';
+            echo 'Ви не вірно обрали діапазон';
+            $_SESSION['try'] = $_SESSION['try'] + 1;
         }
     }
     ?>
--- number.php
+++ number.php
@@ -1,14 +1,10 @@
-<?php 
-session_start();
-if(!$_SESSION['di'] AND $_GET){
-    $_SESSION['di'] = $_GET['di'];
-}
-if($_GET['numbers']){
-    $_SESSION['answers'][count($_SESSION['answers'])] = $_GET['numbers'];
+<?php session_start();
+if($_GET['number']){
+    $_SESSION['answers'][count($_SESSION['answers'])] = $_GET['number'];
 }
 ?>
 <!DOCTYPE html>
-<html lang="uk">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -16,52 +12,47 @@ if($_GET['numbers']){
     <title>Choose number</title>
 </head>
 <body>
-    <?php
-    ?>
-    <form action="./number.php" method="get">
-        <p>Choose number</p>
-        <?php if($_SESSION['try'] <= 3){?>
-            <select name="numbers">
-            <?php for ($i = $_SESSION['di']; $i <= ($_SESSION['di'] + 9); $i++) {
-                        $dis = false; 
-                        for($g = 0; $g < count($_SESSION['answers']); $g++){
-                            if($_SESSION['answers'][$g] == $i){
-                                $dis = true;
-                                break;
-                            }
-                        } ?>
-                    <option  value="<?= $i ?>"><?= $i ?></option>
-            <?php }?>
-        </select>
-        <button type="submit">Choose</button>
-        <?php } ?>
+    <p>Оберіть число</p>
+    <?php if($_SESSION['try'] < 3) { ?>
+        <form action="./number.php" method="get">
+        <select name="number">
+        <?php for($i = $_SESSION['di']; $i <= $_SESSION['di'] + 9; $i++){
+                $dis = false;
+                for($g = 0; $g < count($_SESSION['answers']); $g++) {
+                    if($i == $_SESSION['answers'][$g]){
+                        $dis = true;
+                    }}?>
+        <option <?php if($_GET['number'] AND $dis == true){echo 'disabled';} ?> value="<?= $i ?>"><?= $i ?></option>
+            <?php } ?>
+       
+       </select>
+       <button type="submit">Обрати</button>
     </form>
-    <?php
-     if($_SESSION['try'] > 3){
-        echo "Ви програли";
-        echo '<pre>';
-        echo '<a href="./again.php">Play again</a>';
-    }
-    else if($_GET['numbers']){
-        if($_SESSION['try'] > 3){
-            echo "Ви програли";
+    <?php } ?>
+    <?php 
+    if($_GET){
+        if($_SESSION['try'] >= 3){
+            echo 'Ви прогали.';
             echo '<pre>';
-            echo '<a href="./again.php">Play again</a>';
-        }else if($_SESSION['rand'] === $_GET['numbers']){
-            if($_SESSION['try'] == 1){
-                echo 'Ви справжній екстрасенс';
+            echo '<a href="./again.php">Грати знову</a>';
+        }if($_GET['number'] == $_SESSION['rand']){
+            if($_SESSION['try'] == 0){
+                echo 'Ви справжній екстрасенс.';
             }else{
-                echo 'Ви вгадаои число з '.$_SESSION['try'].' разу';
+                echo 'Ви перемоги з'.($_SESSION['try'] + 1).' разу';
             }
             echo '<pre>';
-            echo '<a href="./again.php">Play again</a>';
-        }else{
-            $_SESSION['try'] = $_SESSION['try'] + 1;
-            if($_SESSION['rand'] < $_SESSION['numbers']){
-                echo 'загадане число менше '.$_SESSION['numbers'].'.';
-            }else if($_SESSION['rand'] > $_SESSION['numbers']){
-                echo 'загадане число більше '.$_SESSION['numbers'].'.';
+            echo '<a href="./again.php">Грати знову</a>';
+        }
+        else if($_SESSION['rand'] !== $_GET['number'] AND $_SESSION['try'] < 3){
+            echo 'Ви обрали не вірне число.';
+            echo '<pre>';
+            if($_GET['number'] < $_SESSION['rand']){
+                echo 'Зашадане число менше'. $_GET['number']. '.';
+            }else if($_GET['number'] > $_SESSION['rand']){
+                echo 'Зашадане число більше'. $_GET['number']. '.';
             }
+            $_SESSION['try'] = $_SESSION['try'] + 1;
         }
     }
     ?>
